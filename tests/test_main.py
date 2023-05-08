b67bcab076b21e3f789be3bb8dbdcb70e317678c
Ahmed Ridene <ahmedridene5@gmail.com>
2023-05-08 17:02:52 +0100

    feat: update `test_apply_filters` func

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,7 +26,27 @@ def test_read_file(spark):
 
 
 @pytest.mark.usefixtures('spark')
-def test_apply_filters(spark_mock_df):
-    input_df, excepted_df = spark_mock_df
+def test_apply_filters(spark):
+    input_columns = [
+        "REPLACING-RENAULT-REF", "REPLACING-SUPPLIER-REF", "REPLACING-SUPPLIER-NAME",
+        "REPLACED-RENAULT-REF",	"REPLACED-SUPPLIER-REF", "REPLACEMENT-DATE", 	
+    ]
+    input_data = [
+        (None, '#N/A', 'SHELL', '8671013783', 'NOUVEAU', '03/11/2014'),
+        (None, '#N/A', 'SHELL', 'xxx', 'NOUVEAU', '03/11/2014'),
+        ('8660000025', '#N/A', 'SHELL', '8671013785', 'NOUVEAU','03/11/2014'),
+        ('8660000710', '437444', 'Valeo', '8671000020', '#N/A','13/02/2019'),
+        ('8660000710', '437444', 'Valeo', '8671000000', '#N/A','13/02/2018'),
+        ('0', '#N/A', 'INCONNU AM', '8671000000', 'CONSOMMABLES','13/02/2014'),
+        ('0', '#N/A', 'AWS', '8671000001', 'CONSOMMABLES', '13/02/2014'),
+        ('8660000712', '#N/A', 'aws', '8671000002', 'CONSOMMABLES','13/02/2014'),
+        ('8660000713', '#N/A', 'CLOUD', '8671000003', 'CONSOMMABLES','13/02/2014')
+    ]
+    input_df = spark.createDataFrame(input_data).toDF(*input_columns)
+    output_columns = input_columns + ["SHIPPING_DATE"]
+    output_data = [("8660000025","EMPTY","SHELL","8671013785","NOUVEAU","03/11/2014","2023"), 
+            ("8660000710","437444","Valeo","8671000000","EMPTY","13/02/2018","2024"), 
+            ("8660000713","EMPTY","CLOUD","8671000003","CONSOMMABLES","13/02/2014","2022")]
+    excepted_df = spark.createDataFrame(output_data).toDF(*output_columns)
     actual_df = transformer.apply_filters(dataset=input_df)
     assert_df_equality(actual_df, excepted_df)
