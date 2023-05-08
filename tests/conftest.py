e2b50338bc52bbdb5e1a1e0a56f8c036f750e165
Ahmed Ridene <ahmedridene5@gmail.com>
2023-05-08 12:04:46 +0100

    refactor: apply changes in tests files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,39 @@
-import pytest
-from src.main import Transformation
+""" Script where we can place our fixtures to structure and arrange our test case data. 
+The fixture can be reusable within a test file. It’s convenient to reuse the same fixture for different test cases. 
+The fixture can be both data like our above mock data function, 
+or it could be a spark environment we would like to initiate for the test session. 
+"""
 
-transformer = Transformation
+import pytest
+from pyspark.sql import SparkSession
 
 @pytest.fixture(scope='session')
 def spark():
-    return transformer.spark
+    return SparkSession.builder.appName("Exercice Pyspark").getOrCreate()
+
+
+@pytest.fixture
+def spark_mock_df(spark):
+
+    input_columns = [
+        "REPLACING-RENAULT-REF", "REPLACING-SUPPLIER-REF", "REPLACING-SUPPLIER-NAME",
+        "REPLACED-RENAULT-REF",	"REPLACED-SUPPLIER-REF", "REPLACEMENT-DATE", 	
+    ]
+    input_data = [
+        (None, '#N/A', 'SHELL', '8671013783', 'NOUVEAU', '03/11/2014'),
+        (None, '#N/A', 'SHELL', 'xxx', 'NOUVEAU', '03/11/2014'),
+        ('8660000025', '#N/A', 'SHELL', '8671013785', 'NOUVEAU','03/11/2014'),
+        ('8660000710', '437444', 'Valeo', '8671000020', '#N/A','13/02/2019'),
+        ('8660000710', '437444', 'Valeo', '8671000000', '#N/A','13/02/2018'),
+        ('0', '#N/A', 'INCONNU AM', '8671000000', 'CONSOMMABLES','13/02/2014'),
+        ('0', '#N/A', 'AWS', '8671000001', 'CONSOMMABLES', '13/02/2014'),
+        ('8660000712', '#N/A', 'aws', '8671000002', 'CONSOMMABLES','13/02/2014'),
+        ('8660000713', '#N/A', 'CLOUD', '8671000003', 'CONSOMMABLES','13/02/2014')
+    ]
+    df = spark.createDataFrame(input_data).toDF(*input_columns)
+    output_columns = input_columns + ["SHIPPING_DATE"]
+    output_data = [("8660000025","EMPTY","SHELL","8671013785","NOUVEAU","03/11/2014","2023"), 
+            ("8660000710","437444","Valeo","8671000000","EMPTY","13/02/2018","2024"), 
+            ("8660000713","EMPTY","CLOUD","8671000003","CONSOMMABLES","13/02/2014","2022")]
+    excepted_df = spark.createDataFrame(output_data).toDF(*output_columns)
+    return df, excepted_df
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,6 @@
 """
 
 import pytest
-# import pandas as pd
 from src.main import Transformation
 from chispa.dataframe_comparer import assert_df_equality
 
@@ -25,30 +24,7 @@ def test_read_file(spark):
 
 
 @pytest.mark.usefixtures('spark')
-def test_apply_filters(spark):
-    input_columns = [
-        "REPLACING-RENAULT-REF", "REPLACING-SUPPLIER-REF", "REPLACING-SUPPLIER-NAME",
-        "REPLACED-RENAULT-REF",	"REPLACED-SUPPLIER-REF", "REPLACEMENT-DATE", 	
-    ]
-    input_data = [
-        (None, '#N/A', 'SHELL', '8671013783', 'NOUVEAU', '03/11/2014'),
-        (None, '#N/A', 'SHELL', 'xxx', 'NOUVEAU', '03/11/2014'),
-        ('8660000025', '#N/A', 'SHELL', '8671013785', 'NOUVEAU','03/11/2014'),
-        ('8660000710', '437444', 'Valeo', '8671000020', '#N/A','13/02/2019'),
-        ('8660000710', '437444', 'Valeo', '8671000000', '#N/A','13/02/2018'),
-        ('0', '#N/A', 'INCONNU AM', '8671000000', 'CONSOMMABLES','13/02/2014'),
-        ('0', '#N/A', 'AWS', '8671000001', 'CONSOMMABLES', '13/02/2014'),
-        ('8660000712', '#N/A', 'aws', '8671000002', 'CONSOMMABLES','13/02/2014'),
-        ('8660000713', '#N/A', 'CLOUD', '8671000003', 'CONSOMMABLES','13/02/2014')
-    ]
-    input_df = spark.createDataFrame(input_data).toDF(*input_columns)
-    output_columns = input_columns + ["SHIPPING_DATE"]
-    data = [("8660000025","EMPTY","SHELL","8671013785","NOUVEAU","03/11/2014","2023"), 
-            ("8660000710","437444","Valeo","8671000000","EMPTY","13/02/2018","2024"), 
-            ("8660000713","EMPTY","CLOUD","8671000003","CONSOMMABLES","13/02/2014","2022")]
-    excepted_df = spark.createDataFrame(data).toDF(*output_columns)
-    # expected_pandas = excepted_df.toPandas()
-    output_df = transformer.apply_filters(dataset=input_df)
-    # output_pandas = output_df.toPandas()
-    # pd.testing.assert_frame_equal(output_pandas, expected_pandas, check_exact=True)
-    assert_df_equality(output_df, excepted_df)
+def test_apply_filters(spark_mock_df):
+    input_df, excepted_df = spark_mock_df
+    actual_df = transformer.apply_filters(dataset=input_df)
+    assert_df_equality(actual_df, excepted_df)
